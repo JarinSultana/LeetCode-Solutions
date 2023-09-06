bca92d66fc32ce87bb5c677c739311cac67e58f2
unknown <jarinsama1@gmail.com>
2023-09-06 16:40:35 +0600

    Contains Duplicate ii

---
+++ reverseLinkedList.java
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while(head != null)
+        {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
